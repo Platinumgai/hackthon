5c0bec0e5854101f4d7661f42679f36ce3c6d570
Platinumgai <nagendla.sunny2021@gmail.com>
2025-08-19 12:27:19 +0530

    model trainer done

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from src.ChestCancer import logger
 from src.ChestCancer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.ChestCancer.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.ChestCancer.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+
+
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -15,3 +19,44 @@ except Exception as e:
     raise e
 
 
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
